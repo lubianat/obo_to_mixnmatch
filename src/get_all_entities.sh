94794b678a6322960a39c7bb57fc68e8ef86cb18
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-01-02 14:04:17 -0300

    Add superclasses

--- src/get_all_entities.sh
+++ src/get_all_entities.sh
@@ -3,12 +3,12 @@ wget -nc -O data/$1.owl  http://purl.obolibrary.org/obo/$1.owl
 
 
 robot export --input data/$1.owl \
-  --header "ID|LABEL|IAO_0000115" \
+  --header "ID|LABEL|IAO_0000115|hasDbXref|subClassOf [ID]" \
   --export results/$1.csv
 
 grep -v "obsolete" results/$1.csv | grep -v ",,"| grep -i $1:  > results/$1_clean.csv
 
-sed -i '1s/^/id,name,description\n /' results/$1_clean.csv
+sed -i '1s/^/id,name,description,xrefs,parents\n /' results/$1_clean.csv
 
 upperstr=$(echo $1 | tr '[:lower:]' '[:upper:]')
 
